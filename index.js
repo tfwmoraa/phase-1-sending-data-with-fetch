781c2953654f7ccc91f9343febbef3a8264a2e1a
tfwmoraa <jinnettemoraa@gmail.com>
2025-09-19 14:53:59 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    const userData = {
+        name: name,
+        email:email,
+    };
+
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    })
+
+    .then((response) => response.json())
+    .then((data) => {
+        document.body.innerHTML +=  `<p>User ID: ${data.id}</p>`
+    })
+    .catch((error) => {
+        document.body.innerHTML +=  `<p> Error: ${error.message}</p>`
+    });
+}
